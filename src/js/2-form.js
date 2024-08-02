7509a25ca6f9b8b349e4884eb0468abc82ea63a3
Ivan_Bog <24term@gmail.com>
2024-08-02 09:41:52 +0300

    edit form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -4,7 +4,7 @@ let formData = {
   // message: '',
 };
 
-const fillFormFields = () => { 
+const fillFormFields = () => {
   const formDataFromLS = JSON.parse(localStorage.getItem(`feedback-form-state`));//зчитуємо дані з localStorage під ключем feedback-form-state
   // console.log(formDataFromLS);
 
@@ -20,10 +20,10 @@ const fillFormFields = () => {
       form.elements[key].value = formDataFromLS[key]; //звертаємось до форми, до псевдомассива elements до елемента змінної key,  і записуємо в цей key значення value = звертаємось до котрий перебираємо, по ключю key і записуємо значення котре знаходилось в localStorage
     }
   }//перебираємо об'єкт formDataFromLS, маємо доступ до ключа в змінній key, робимо перевірку на власні чи не власні властивості
-}
+};
 fillFormFields();
 
-const onFormFielChange = event => { 
+const onFormFielChange = event => {
   const fieldName = event.target.name; //зчитуємо значення атрибута name
   const fieldValue = event.target.value;//зчитуємо значення яке користувач ввів в поле для введення
 
@@ -34,20 +34,21 @@ const onFormFielChange = event => {
   // console.log(formData);
 
   localStorage.setItem(`feedback-form-state`, JSON.stringify(formData));  //записуємо об'єкт formData в localStorage під ключем feedback-form-state в рядковому представленні
-}
+};
 
-const onFeedBackFormSubmit = event => { 
+const onFeedBackFormSubmit = event => {
   event.preventDefault();
 
   if (!formData.email || !formData.message) {
-      alert('Fill please all fields');
-      return;
-    }// Якщо будь-яке з полів (властивостей об’єкта formData) порожнє, показуй сповіщення з текстом «Fill please all fields».
-      console.log(formData);
+    alert('Fill please all fields');
+    return;
+  }// Якщо будь-яке з полів (властивостей об’єкта formData) порожнє, показуй сповіщення з текстом «Fill please all fields».
+  console.log(formData);
     
   event.target.reset();// очищаємо форму
   localStorage.removeItem(`feedback-form-state`);// очищаємо значення в localStorage з ключем feedback-form-state
-}
+  formData = {};
+};
 
 form.addEventListener('input', onFormFielChange);
 form.addEventListener('submit', onFeedBackFormSubmit);
